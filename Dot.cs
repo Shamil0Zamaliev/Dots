0ff482942a316d95e40dcdac4a63b5cffc1cd4d1
Shamil0Zamaliev <114811430+Shamil0Zamaliev@users.noreply.github.com>
2022-10-01 18:52:05 +0300

    Initial commit

---
+++ Dot.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dots
+{
+    internal class Dot
+    {
+        private double x;
+        private double y;
+        private double z;
+
+        public double X { get { return x; } set { x = value; } }
+        public double Y { get { return y; } set { y = value; } }
+        public double Z { get { return z; } set { z = value; } }
+
+        public Dot(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"({X},{Y},{Z})");
+        }
+
+
+        public double Dist0 () 
+        {
+            return Math.Sqrt((x*x) + (y*y) + (z*z));
+        }
+        
+        public static double Distance_between(Dot a, Dot b)
+        {
+            return Math.Sqrt(Math.Pow((a.x - b.x), 2) + Math.Pow((a.y - b.y), 2) + Math.Pow((a.z - b.z), 2));
+        }
+
+        public static Dot operator +(Dot a, Dot b)
+        {
+            Dot c = new (a.x+b.x, a.y + b.y, a.z + b.z);
+            return c;
+        }
+
+        public static void straigh_line_equation (Dot a, Dot b )
+        {
+            Console.WriteLine($"(X - {a.x})/{b.x - a.x} = (Y - {a.y})/{b.y - a.y} = (Z- {a.z})/{b.z - a.z}");
+        }
+
+        public static void plane_equation(Dot a, Dot b, Dot c)
+        {
+           double A, B, C, D;
+            A = a.y* (b.z - c.z) + b.y* (c.z - a.z) + c.y* (a.z - b.z); 
+            B = a.z* (b.x - c.x) + b.z* (c.x - a.x) + c.z* (a.x - b.x); 
+            C = a.x* (b.y - c.y) + b.x* (c.y - a.y) + b.x* (a.y - b.y);
+            D =-(a.x* (b.y* c.z - c.y* b.z) + b.x* (c.y* a.z - a.y* c.z) + c.x* (a.y* b.z - b.y* a.z));
+            Console.WriteLine($"({A}x) + ({B}y) + ({C}z) + ({D}) = 0");
+        }
+
+        public static double Scalar(Dot a, Dot b)
+        {
+            return (a.x * b.x + a.y * b.y + a.z * b.z);
+        }
+
+        public static Dot operator *(Dot a, Dot b)
+        {
+            Dot c = new ((a.y*b.z-a.z*b.y), (a.x*b.z-a.z*b.x), (a.x*b.y-a.y*b.x));
+            return c;
+        } 
+        
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Dots;
+
+Dot d1 = new (1, 2, 3);
+Dot d2 = new (1, 2, 3);
+d1.X = 10;
+d2.Y = 3;
+Console.WriteLine(d1.Dist0());
+Console.WriteLine(Dot.Distance_between(d1, d2));
+Dot d3 = d1 + d2;
+Dot d4 = d1 * d3;
+d1.Show();
+d2.Show();
+d3.Show();
+d4.Show();
+Dot.straigh_line_equation(d1, d3);
+Dot.plane_equation(d1, d2, d3);
+
+
+
+
+
